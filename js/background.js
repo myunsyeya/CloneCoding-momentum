addca1e659bdd391e2c6592a0f653c226b92d2e1
myunsyeya <myunsyeya@ajou.ac.kr>
2023-09-17 23:27:20 +0900

    bgimage 오류 수정

--- js/background.js
+++ js/background.js
@@ -3,5 +3,3 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 document.body.style.backgroundImage = `url('img/${chosenImage}')`;
-
-document.body.appendChild(bgImage)
